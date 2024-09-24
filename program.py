10a6618bb832dd4319e801378427a47a75f96f4a
Landon Lamie <landon.m.lamie@gmail.com>
2024-09-24 17:38:46 -0400

    Write notes for transpose, continue to work on recursive method.

--- program.py
+++ program.py
@@ -37,7 +37,7 @@ def main(*args):
     maze_solver = MazeSolver()
 
     # Create the second maze by transposing the first maze
-    maze2 = transpose_maze(maze1) # May need to make a copy of this list ([:])
+    maze2 = transpose_maze(maze1)  # May need to make a copy of this list ([:])
 
     # Solve the original maze
     maze_solver.solve_maze(maze1, X_START, Y_START)
@@ -47,25 +47,6 @@ def main(*args):
 
 
 def transpose_maze(maze_to_transpose):
-    """This method will take in a 2d list (list of lists) and return a new
-    2d list maze that is flipped along the diagonal, or in mathematical terms,
-    transposed.
-
-    ie. If the array looks like:
-    1, 2, 3
-    4, 5, 6
-    7, 8, 9
-    Then the returned result will be:
-    1, 4, 7
-    2, 5, 8
-    3, 6, 9
-
-    The current return statement is just a placeholder so the program doesn't
-    contain any syntax errors. You need to replace this with the work you need
-    to do for actually transposing the maze.
-
-    It is important that you make a new 2d list and copy each element from the
-    original to the new transposed one. Failure to do so may lead you to only
-    be able to solve the transposed one.
-    """
+    # X become Y and Y becomes X.  0,0 stays.  1,0 becomes 0,1.  1,2 becomes 2,1
+    maze_transposed = []
     return []
